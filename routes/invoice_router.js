ee139dd05c5a78c464ed0ecbfd681093272dc183
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-11-22 18:04:55 +0530

    first commit

---
+++ routes/invoice_router.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { createInvoiceRouter, deleteInvoiceRouter, getByIdInvoiceRouter, getInvoiceRouter, getRemainingDeliveryDays, updateInvoiceRouter, UpdateOrderStatus } from '../controller/invoice_controller.js';
+
+
+const invoice_Router = express.Router();
+
+
+
+
+invoice_Router.get('/payment/invoice',getInvoiceRouter);
+invoice_Router.get('/payment/invoice/:id',getByIdInvoiceRouter);
+invoice_Router.post('/payment/invoice',createInvoiceRouter);
+invoice_Router.put('/payment/invoice/:id',updateInvoiceRouter);
+invoice_Router.delete('/payment/invoice/:id',deleteInvoiceRouter);
+invoice_Router.put('/payment/order/:invoiceId/status',UpdateOrderStatus);
+invoice_Router.get('/payment/invoices/:invoiceId/remaining-days',getRemainingDeliveryDays);
+
+
+
+export default invoice_Router;
